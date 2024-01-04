39d23e74badf6d07561210705e5e46dc4c911f96
stephensinaga <155416238+stefansinaga@users.noreply.github.com>
2024-01-04 10:58:08 +0700

    buat login database

---
+++ coba/database/migrations/2024_01_04_034210_create_iurans_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('iurans', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->string('iuran_a');
+            $table->integer('jumlah');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('iurans');
+    }
+};
--- coba/routes/web.php
+++ coba/routes/web.php
@@ -13,6 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::controller(Authcontroller::class)->group(function () {
+	Route::get('register', 'register')->name('register');
+	Route::post('register', 'registerSimpan')->name('register.simpan');
+
+	Route::get('login', 'login')->name('login');
+	Route::post('login', 'loginAksi')->name('login.aksi');
+
+	Route::get('logout', 'logout')->middleware('auth')->name('logout');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
